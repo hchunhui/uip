73b7cd482efc80ecc8610b8e12aa4b4223ac2d26
Chunhui He <hchunhui@mail.ustc.edu.cn>
2016-05-17 12:37:07 +0000

    fix

--- uip/uip_arp.c
+++ uip/uip_arp.c
@@ -230,12 +230,11 @@ uip_arp_update(u16_t *ipaddr, struct uip_eth_addr *ethaddr)
  * variable uip_len.
  */
 /*-----------------------------------------------------------------------------------*/
-#if 0
 void
 uip_arp_ipin(void)
 {
   uip_len -= sizeof(struct uip_eth_hdr);
-	
+#if 0
   /* Only insert/update an entry if the source IP address of the
      incoming IP packet comes from a host on the local network. */
   if((IPBUF->srcipaddr[0] & uip_netmask[0]) !=
@@ -247,10 +246,9 @@ uip_arp_ipin(void)
     return;
   }
   uip_arp_update(IPBUF->srcipaddr, &(IPBUF->ethhdr.src));
-  
+#endif
   return;
 }
-#endif /* 0 */
 /*-----------------------------------------------------------------------------------*/
 /**
  * ARP processing for incoming ARP packets.
